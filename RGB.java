0b004fb27c119d0e74a9ec49ce10bea75e82bd73
isaac-conceicao <60546623+isaac-conceicao@users.noreply.github.com>
2021-03-18 12:00:01 -0300

    Update RGB.java
    - Inclusão do método getHex() da qestão VIII;
    - Alteração de tipo nos atributos r, g e b para inteiro;
    - Adequação de tipo nos métodos get/set dos atributos r, g e b.

--- RGB.java
+++ RGB.java
@@ -1,36 +1,41 @@
 public class RGB
 {
-    private float red;
-    private float blue;
-    private float green;
+    private int red;
+    private int blue;
+    private int green;
     
-    public float getR(){
+    public int getR(){
         return this.red;
     }
     
-    public void setR(float r){
+    public void setR(int r){
         this.red = r;
     }
     
-    public float getG(){
+    public int getG(){
         return this.green;
     }
     
-    public void setG(float g){
+    public void setG(int g){
         this.green= g;
     }
     
-    public float getB(){
+    public int getB(){
         return this.blue;
     }
     
-    public void setB(float b){
+    public void setB(int b){
         this.blue = b;
     }
     
     int getLuminosidade(){
         double luminosidade = (this.getR()*0.3 + this.getG()*0.59 + this.getB()*0.11)/255;
         return (int) Math.round(luminosidade);
+    }
+    
+    public String getHex(){
+        String hex = "#"+Integer.toHexString(this.getR())+Integer.toHexString(this.getG())+Integer.toHexString(this.getB());
+        return hex.toUpperCase();
     }
 }
 
