e908107e3c42fcbbf1abfea1399eda28ab78cb45
João Pedro <betajpsg@gmail.com>
2021-03-23 15:12:45 -0300

    Update RGB.java
    Organização do código e correção das funções verificarCor e getLuminosidade

--- RGB.java
+++ RGB.java
@@ -1,56 +1,40 @@
-public class RGB
-{
+public class RGB {
     private int red;
     private int blue;
     private int green;
     
-    public  RGB(RGB cor){//Construtor Copia
-        
-        this.red=cor.getR();
-        this.blue=cor.getB();
-        this.green=cor.getG();
-    
+    public RGB(RGB cor) {//Construtor Copia
+        this.red = cor.getR();
+        this.blue = cor.getB();
+        this.green = cor.getG();
     }
-    public  RGB(){ //Construtor Cor Preta
-        
+    
+    public RGB() { //Construtor Cor Preta
         this.red = 0;
         this.blue = 0;
         this.green = 0;
-    
     }
-    public  RGB (int r , int b, int g){ //Construtor CriarCor
-        
+    
+    public RGB (int r , int b, int g) { //Construtor CriarCor
         this.red = r;
         this.blue = b;
         this.green = g;
-        
-    }
-    public int getR(){
-        return this.red;
     }
     
-    public void setR(int r){
-        this.red = r;
+    public int getR(){
+        return this.red;
     }
     
     public int getG(){
         return this.green;
     }
     
-    public void setG(int g){
-        this.green= g;
-    }
-    
     public int getB(){
         return this.blue;
     }
     
-    public void setB(int b){
-        this.blue = b;
-    }
-    
-    int getLuminosidade(){
-        double luminosidade = (this.getR()*0.3 + this.getG()*0.59 + this.getB()*0.11)/255;
+    public int getLuminosidade() {
+        double luminosidade = (this.getR()*0.3 + this.getG()*0.59 + this.getB()*0.11);
         return (int) Math.round(luminosidade);
     }
     
@@ -58,11 +42,10 @@ public class RGB
         String hex = "#"+Integer.toHexString(this.getR())+Integer.toHexString(this.getG())+Integer.toHexString(this.getB());
         return hex.toUpperCase();
     }
-         public void VerificarCor(RGB cor2){
     
-        if(this.red == cor2.getR() && this.blue == cor2.getB() && this.green == cor2.getG() ){
-        System.out.println("A COR SAO IGUAIS");
+    public void verificarCor(RGB cor2) {
+        if(this.getR() == cor2.getR() && this.getB() == cor2.getB() && this.getG() == cor2.getG()) {
+            System.out.println("A CORES SAO IGUAIS");
         }
     }
 }
-
