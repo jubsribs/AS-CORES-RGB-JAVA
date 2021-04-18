26b5ae92ade64ff4f25bf345b8bb66f538d5482c
isaac-conceicao <60546623+isaac-conceicao@users.noreply.github.com>
2021-04-18 19:02:58 -0300

    Update Imagem.java (#10)
    - Inclusão dos atributos altura e largura na classe;
    - Inclusão de construtor com cópia/clone;
    - Inclusão dos métodos getAltura(), getLargura(), getPixel(), criaNovaImagemCinza(), greyScale() e girar90Graus();
    - Adequação nos métodos para utilização dos atributos altura/largura;
    - Adequação nos métodos para utiliação de getAltura() e getLargura().

--- Imagem.java
+++ Imagem.java
@@ -1,16 +1,65 @@
 public class Imagem {
     private RGB imagem[][];
-    private RGB pixel; 
+    private RGB pixel;
+    private int altura;
+    private int largura;
     
     
     public Imagem(int altura, int largura) {
         this.imagem = new RGB[altura][largura];
-        
+        this.altura=altura;
+        this.largura=largura;
+
         for(int i = 0; i< altura; i++) 
             for(int j=0; j<largura; j++) 
                 this.imagem[i][j] = this.pixel; //preciso do estático branco de rgb
     }
-   
+    
+    //kage bunshin no jutso!
+    public Imagem(Imagem original) {
+        int altura = original.getAltura();
+        int largura = original.getLargura();
+        
+        //cria imagem com as mesmas dimensões
+        this.altura = altura;
+        this.largura = largura;
+        this.imagem = new RGB[altura][largura];
+        
+        //copia conteúdo da original
+        for(int i = 0; i< altura; i++) 
+            for(int j=0; j<largura; j++)
+                this.imagem[i][j] = new RGB(original.getPixel(i,j));
+    }
+
+    public int getAltura() {
+        return this.altura;
+    }
+
+    public int getLargura() {
+        return this.largura;
+    }
+
+    //retorna o pixel da posição indicada
+    public RGB getPixel(int x, int y){
+        //FALTAM VALIDAÇÕES
+        return this.imagem[x][y];
+    }
+
+    //cria uma nova imagem identica e converte na escala de cinza
+    public Imagem criaNovaImagemCinza() {
+        Imagem imagemCinza = new Imagem(this);
+
+        imagemCinza.greyScale();
+        
+        return imagemCinza;
+    }
+
+    //converte cada pixel da imagem atual ao seu equivalente na escala de cinza
+    public void greyScale() {
+        for(int i = 0; i< altura; i++) 
+            for(int j=0; j<largura; j++)
+                this.imagem[i][j].turnGrey();
+    }
     
     public void modificaPixelImagem(int x, int y, RGB pixel) {
         if(verificaSeDimensoesPassadasSeEnquandramNaImagem(1, 2))
@@ -29,8 +78,6 @@ public class Imagem {
         return false;
     }
     
-    
-    
     public boolean verificaSeEhFragmento(Imagem img1, Imagem img2) {
         
     }
@@ -38,8 +85,8 @@ public class Imagem {
     
     //nesse caso vou passar 2 img, mas poderia passar um vetor de imgs
     private boolean comparaPixels(Imagem img1, Imagem img2) {
-        for(int i = 0; i< img1.imagem.length; i++) {
-            for(int j = 0; j< img1.imagem[0].length; j++)
+        for(int i = 0; i< img1.getAltura(); i++) {
+            for(int j = 0; j< img1.getLargura(); j++)
                 if(img1.imagem[i][j].getHex() != img2.imagem[i][j].getHex()) //opcao do equals
                     return false;
         }
@@ -49,20 +96,35 @@ public class Imagem {
     
     //nesse caso vou passar 2 img, mas poderia passar um vetor de imgs
     private boolean comparaDimensaoImagens(Imagem img1, Imagem img2) {
-        if(img1.imagem.length == img2.imagem.length 
-            && img1.imagem[0].length == img2.imagem[0].length)
+        if(img1.getAltura() == img2.getAltura() 
+            && img1.getLargura() == img2.getLargura())
             return true;
         return false;
     }
     
-    
     private boolean verificaSeDimensoesPassadasSeEnquandramNaImagem(int x, int y){
-        int tamanhoHorizontal = this.imagem.length;
-        int tamanhoVertical = this.imagem[0].length;
+        int tamanhoHorizontal = this.getAltura();
+        int tamanhoVertical = this.getLargura();
         
         if(x > tamanhoHorizontal || y > tamanhoVertical)
             return false;
         
         return true;
+    }
+
+    private void girar90Graus() {//gira a imagem 90° à esquerda\anti-horário
+        Imagem imagemClone = new Imagem(this);
+
+        int altura = this.getAltura();
+        int largura = this.getLargura();
+        int k = 0;
+        int l = largura-1;
+        for(int i = 0; i< altura; i++) {
+            for(int j=0; j<largura; j++){
+                this.imagem[i][j]= new RGB(imagemClone.getPixel(k,l));
+                k++;
+            }
+            l--;
+        }
     }
 }
